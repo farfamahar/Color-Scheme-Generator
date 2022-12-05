b02e915d26d999230a655cc32d4b00c478a780ac
Farfama <farfama.hargaaya@gmail.com>
2022-12-06 02:15:47 +0900

    Update fetch colors from color api

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-import './style.css'
 
-function refreshColor(){
-  console.log("here")
-  let color = document.getElementById("color-selector").value
-console.log(color)
-  document.querySelector('#app').innerHTML = `
-  <div>
-  <input type="color" value="${color}"/>
-  </div>
-  ` 
-}
-
-refreshColor()
